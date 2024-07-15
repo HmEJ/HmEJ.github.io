7c65d3f431dd9ddecbc4a98172feaed7de6df652
ahang <mhangggggg@outlook.com>
2024-07-15 21:26:26 +0800

    Site updated: 2024-07-15 21:26:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/24/article1/","2024/07/07/article4/","2024/06/26/article3/","2024/06/25/article2/"];function toRandomPost(){
+var posts=["2024/06/25/article2/","2024/06/24/article1/","2024/06/26/article3/","2024/07/07/article4/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
